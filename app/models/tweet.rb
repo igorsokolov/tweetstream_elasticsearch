3823615fc7825f3b2d8864b511229ab56e969462
Igor Sokolov <igor.sokolov@gmail.com>
2016-01-31 11:01:42 -0800

    Added User information to tweet. Added search with parameters

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,11 @@
+class User
+  include Virtus.model
+
+  attribute :screen_name
+  attribute :name
+  attribute :profile_image_url
+end
+
 class Tweet
   include Elasticsearch::Persistence::Model
 
@@ -6,12 +14,77 @@ class Tweet
   index_name SEARCH_ALIAS
 
   attribute :tweet_id,    String
-  attribute :text,        String,                   mapping: { type: 'string', analyzer: 'keyword' }
-  attribute :location,    Array,                    mapping: { type: 'geo_point' }
+  attribute :text,        String,                   mapping: { type: 'string', analyzer: 'snowball' }
+  attribute :location,    Array,                    mapping: { 
+                                                      type: 'geo_point', 
+                                                      geohash: true, 
+                                                      geohash_prefix: true,
+                                                      geohash_precision: '1mi'
+                                                    }
   attribute :created_at,  Time, default: Time.now,  mapping: { type: 'date' }
+  attribute :user,        User,                     mapping: { type: 'object' }
 
-  def self.search(*args)
-    self.index_name = SEARCH_ALIAS
-    super
+  def self.search(search_string, lon, lat, radius)
+    query_part =  if search_string.empty?
+                    { match_all: {} }
+                  else
+                    { 
+                      simple_query_string: {
+                        query: search_string
+                      }
+                    }
+                  end
+    query = { 
+      query: { 
+        bool: { 
+          must: [ 
+            query_part
+          ], 
+          filter: { 
+            geo_distance: {
+              distance: "#{radius.to_i}mi",
+              location: { lon: lon.to_f, lat: lat.to_f }
+            }
+          }
+        }
+      }, 
+      sort: {
+        created_at: {
+          order: "desc" 
+        }
+      }, 
+      size: 250
+    }
+    begin
+      self.index_name = SEARCH_ALIAS
+      p query
+      super(query)
+    rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
+      Rails.logger.fatal("Search error: #{e.message}")
+    end
   end
 end
+
+{ 
+  query: { 
+    bool: { 
+      must: [ 
+        { 
+          match_all:{} 
+        } 
+      ], 
+      filter: { 
+        geo_distance: {
+          distance:'100mi',
+          location: { lon: 122.68, lat: 37.75 }
+        }
+      }
+    }
+  }, 
+  sort: {
+    created_at: {
+      order: "desc" 
+    }
+  }, 
+  size: 250
+}
